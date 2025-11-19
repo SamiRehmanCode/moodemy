3803b689f09e482abe40a3ebbbe1c7b43836afb6
AbdulRehmanasghar <hsn.graphics007@gmail.com>
2025-11-19 17:59:27 +0500

    refactor: remove BannersPage component and update ESLint configuration

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,7 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+
   {
     ignores: [
       "node_modules/**",
@@ -19,6 +20,15 @@ const eslintConfig = [
       "build/**",
       "next-env.d.ts",
     ],
+  },
+
+  // ⭐ Add rules here
+  {
+    rules: {
+      "react/no-unescaped-entities": "off",
+      "@typescript-eslint/no-unused-vars": "off",
+      "react-hooks/exhaustive-deps": "off",
+    },
   },
 ];
 
